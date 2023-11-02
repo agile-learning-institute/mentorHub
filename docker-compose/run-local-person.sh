33d17d47aed70fdb48f843651e64d1ef9803672c
Mike Storey <mike@ssgpp.com>
2023-11-02 16:39:11 -0400

    fixed curl curl error

--- docker-compose/run-local-person.sh
+++ docker-compose/run-local-person.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/local
 cd ~/local
 docker compose down
-curl curl https://raw.githubusercontent.com/agile-learning-institute/institute/main/docker-compose/docker-compose-person.yaml > docker-compose.yaml
+curl https://raw.githubusercontent.com/agile-learning-institute/institute/main/docker-compose/docker-compose-person.yaml > docker-compose.yaml
 docker compose up --detach
