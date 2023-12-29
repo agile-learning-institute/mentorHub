f9c0f0f2c536688f564bfc56f8d2f364bf51b4f0
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2023-12-29 16:13:49 -0500

    Unify scripts using Docker Compose Profiles

--- docker-configurations/run-local.sh
+++ docker-configurations/run-local.sh
@@ -1,3 +1,7 @@
+if [ ! -z "$1" ]; then
+	export COMPOSE_PROFILES="$1"
+fi
+
 docker compose down
 curl --create-dirs --output-dir ~/local https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-configurations/docker-compose.yaml
 docker compose --project-directory ~/local up --detach
