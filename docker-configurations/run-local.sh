08cc02936a9885f0421255bbe15062cb78f30da6
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2024-02-16 12:11:53 -0500

    Make run-local self-cleaning

--- docker-configurations/run-local.sh
+++ docker-configurations/run-local.sh
@@ -2,6 +2,22 @@ if [ ! -z "$1" ]; then
 	export COMPOSE_PROFILES="$1"
 fi
 
-docker compose --project-directory ~/local down
-curl --create-dirs --output ~/local/docker-compose.yaml https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-configurations/docker-compose.yaml
-docker compose --project-directory ~/local up --detach
+compose_dir="$(mktemp -d)"
+
+docker compose --project-directory "$compose_dir" down
+curl --create-dirs --output "$compose_dir/docker-compose.yaml" https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-configurations/docker-compose.yaml
+docker compose --project-directory "$compose_dir" up --detach && {
+
+
+cat > compose-down.sh << EOF
+#!/bin/sh
+
+docker compose --project-directory "$compose_dir" --profile "$1" down && rm -r "$compose_dir" && rm "\$0"
+EOF
+
+
+chmod u+x compose-down.sh
+
+echo "To stop the containers, just execute compose-down.sh"
+
+}
