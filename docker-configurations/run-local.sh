f35705b21b55b7c28713bfeb0b04de8a731157ff
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2024-02-16 12:39:35 -0500

    Remove non-functional compose down command
    This will always fail because a new compose_dir is created each time
    the script is run

--- docker-configurations/run-local.sh
+++ docker-configurations/run-local.sh
@@ -4,7 +4,6 @@ fi
 
 compose_dir="$(mktemp -d)"
 
-docker compose --project-directory "$compose_dir" down
 curl --create-dirs --output "$compose_dir/docker-compose.yaml" https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-configurations/docker-compose.yaml
 docker compose --project-directory "$compose_dir" up --detach && {
 
